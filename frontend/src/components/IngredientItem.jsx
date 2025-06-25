aeb5c4dbc06a739bcd24d78ae64a5b61b94c73fe
Abhinav-2223 <narneabhinav123@gmail.com>
2025-06-26 03:40:48 +0800

    settled NaN issue

--- frontend/src/components/IngredientItem.jsx
+++ frontend/src/components/IngredientItem.jsx
@@ -3,17 +3,19 @@ import React from 'react';
 import './IngredientItem.css'; // Import the CSS file directly
 
 const IngredientItem = ({ ingredient, servingSize }) => {
-  const { name, type, image, quantity, unit } = ingredient;
-  const scaledQuantity = quantity * servingSize;
+  const { name, type, image, qty, unit } = ingredient;
+  const intServingSize = parseInt(servingSize) || 1;
+  const parsedQty = parseFloat(qty) || 0;
+  const scaledQty = parsedQty * intServingSize;
 
   return (
     <div className="ii-item"> {/* Use the prefixed class name */}
-      <img src={image} alt={name} className="ii-image" /> {/* Use the prefixed class name */}
+      {/* <img src={image} alt={name} className="ii-image" /> Use the prefixed class name */}
       <div className="ii-info"> {/* Use the prefixed class name */}
         <div className="ii-name">{name.toUpperCase()}</div> {/* Use the prefixed class name */}
       </div>
       <div className="ii-quantity"> {/* Use the prefixed class name */}
-        {unit === 'count' ? `x ${scaledQuantity}` : `${scaledQuantity} ${unit}`}
+        {unit === 'count' ? `x ${scaledQty}` : `${scaledQty} ${unit}`}
       </div>
     </div>
   );
